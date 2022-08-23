3c5d6ba5d73da57b3f5ef1d17b1b1c2360bb0145
haemin <goals1005@naver.com>
2022-08-23 16:42:57 +0900

    NamedQuery 사용해보기(잘 사용하지 않는 기능)

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -10,6 +10,10 @@ import static lombok.AccessLevel.*;
 @Getter @Setter
 @NoArgsConstructor(access = PROTECTED) //접근제어자 복습: 같은 패키지, 다른 패키지 자손까지
 @ToString(of = {"id", "username", "age"})
+@NamedQuery( //잘 사용하지 않음 (하지만 장점: 쿼리 오타시 애플리케이션 실행시점에 오류 알려준다.(creatQuery: 오타가 있어도 그냥 실행됨 > 오류 찾기 힘들다.)
+        name="Member.findByUsername",
+        query="select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -88,4 +88,15 @@ class MemberRepositoryTest {
         List<Member> helloBy = memberRepository.findTop3HelloBy();
     }
 
+    @Test
+    void namedQuery() {
+        Member member1 = new Member("AAA", 10);
+        Member member2 = new Member("AAA", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByUsername("AAA");
+        Member findMember = result.get(0);
+        assertThat(findMember).isEqualTo(member1);
+    }
 }
